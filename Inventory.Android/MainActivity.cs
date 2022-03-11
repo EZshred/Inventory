bfe81c346f99c8020e817fa20af3283fad0f736d
EZshred <rray@EZshred.com>
2022-03-11 03:45:04 -0500

    More zebra scanning changes
    Hardware and software scan working at floor level.   Still getting multiple barcodes though.

--- Inventory.Android/MainActivity.cs
+++ Inventory.Android/MainActivity.cs
@@ -15,7 +15,7 @@ namespace Inventory.Droid
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            ToolbarResource = Resource.Layout.Toolbar;  
 
             base.OnCreate(bundle);
 
